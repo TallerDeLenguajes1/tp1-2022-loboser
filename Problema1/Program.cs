aebefc8df61bcc7c5c193053300e9e5598f7044f
Zair <loboser.sl@gmail.com>
2022-08-27 13:43:40 -0300

    problema 1 y 2

---
+++ Problema1/Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Problema1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num;
+            Console.WriteLine("Ingresar un numero: ");
+            try
+            {
+                num = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(num*num);
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Error de formato (" + ex.Message + ")");
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine("Error de desbordamiento (" + ex.Message + ")");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Problema2/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+
+namespace Problema2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1,num2;
+            try
+            {
+                Console.Write("Ingresar dividendo: ");
+                num1 = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Ingresar divisor: ");
+                num2 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Resultado: " + num1/num2);
+            }
+            
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Error de division por 0 (" + ex.Message + ")");
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine("Error de formato (" + ex.Message + ")");
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine("Error de desbordamiento (" + ex.Message + ")");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
