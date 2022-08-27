9b42883560173b96d840f09d285327515079a0b9
Zair <loboser.sl@gmail.com>
2022-08-27 14:39:25 -0300

    4

---
+++ Problema4/Provincias.cs
@@ -0,0 +1,24 @@
+// Root myDeserializedClass = JsonConvert.DeserializeObject<Root>(myJsonResponse);
+
+namespace Problema4
+{    
+    public class Parametros
+    {
+        public List<string> campos { get; set; }
+    }
+
+    public class Provincia
+    {
+        public string id { get; set; }
+        public string nombre { get; set; }
+    }
+
+    public class Root
+    {
+        public int cantidad { get; set; }
+        public int inicio { get; set; }
+        public Parametros parametros { get; set; }
+        public List<Provincia> provincias { get; set; }
+        public int total { get; set; }
+    }
+}
